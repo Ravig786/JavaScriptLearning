8d6d287f3272c62d9fd198c4453e13cdcb01797b
Ravig786 <ravig8233@gmail.com>
2021-07-07 10:50:31 +0530

    operators

---
+++ 1.Js Basics/3.Operators/4.Bitwise.js
@@ -0,0 +1,8 @@
+// Operator	    Description	      Example	    Same as	        Result	    Decimal
+// &	           AND	          5 & 1	        0101 & 0001	     0001	        1
+// |	           OR	          5 | 1	        0101 | 0001	     0101	        5
+// ~	           NOT	          ~ 5	        ~0101	         1010	        10
+// ^	           XOR	          5 ^ 1	        0101 ^ 0001	     0100	        4
+// <<	    Zero fill left shift	5 << 1	     0101 << 1	     1010	        10
+// >>	    Signed right shift	    5 >> 1	     0101 >> 1	     0010	         2
+// >>>	    Zero fill right shift	5 >>> 1	     0101 >>> 1	     0010	         2
