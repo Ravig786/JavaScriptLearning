4f61c38afa428ea8c5433c6635ade2972022864b
Ravig786 <ravig8233@gmail.com>
2021-07-07 10:51:01 +0530

    operators

---
+++ 1.Js Basics/3.Operators/1.Arithmetic.js
@@ -0,0 +1,9 @@
+// Operator	Description
+// +	Addition
+// -	Subtraction
+// *	Multiplication
+// **	Exponentiation (ES2016)
+// /	Division
+// %	Modulus (Division Remainder)
+// ++	Increment
+// --	Decrement
---
+++ 1.Js Basics/3.Operators/2.Assignment.js
@@ -0,0 +1,8 @@
+// Operator	Example	Same As
+// =	x = y	x = y
+// +=	x += y	x = x + y
+// -=	x -= y	x = x - y
+// *=	x *= y	x = x * y
+// /=	x /= y	x = x / y
+// %=	x %= y	x = x % y
+// **=	x **= y	x = x ** y
