812330bc856f144391a58044fbb616a02d34f993
Ravig786 <ravig8233@gmail.com>
2021-07-07 15:35:22 +0530

    timer

---
+++ 3.Timers/1.setTimeOut/3.setTimeOutWithFunctionAndArguments.js
@@ -0,0 +1,5 @@
+const myFunc = (param1, param2) => {
+    console.log(param1 + param2 + ". It rocks");
+};
+
+setTimeout(myFunc, 2 * 1000, "Timers", " are great");
---
+++ 3.Timers/1.setTimeOut/4.example.js
@@ -0,0 +1,6 @@
+const myFunc = (delay) => {
+    console.log("Hello after" + delay + " seconds");
+};
+
+setTimeout(myFunc, 5 * 1000, "five");
+setTimeout(myFunc, 10 * 1000, "ten");
