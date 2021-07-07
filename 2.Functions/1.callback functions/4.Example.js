50e468afcc0ce863da86143604da8d95590af886
Ravig786 <ravig8233@gmail.com>
2021-07-07 12:10:17 +0530

    callback functions

---
+++ 2.Functions/1.callback functions/4.Example.js
@@ -0,0 +1,21 @@
+const posts = [{
+        title: "Post One",
+        body: "This is post one",
+    },
+    {
+        title: "Post Two",
+        body: "This is post two",
+    },
+];
+
+function getPosts() {
+    setTimeout(() => {
+        let output = "";
+        posts.forEach((post, index) => {
+            output += `<li>${post.title}</li>`;
+        });
+        document.body.innerHTML = output;
+    }, 2000);
+}
+
+getPosts();
